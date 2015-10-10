63ee2810c55e46d8500c15311c14a4c91b483375
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-10-10 10:11:34 +0100

    Cacher: test interface.

---
+++ cacher_test.go
@@ -0,0 +1,20 @@
+package cacher_test
+
+import (
+	"testing"
+
+	"github.com/jelmersnoeck/cacher"
+)
+
+func acceptCacher(c cacher.Cacher) bool {
+	return true
+}
+
+func TestMemoryCache(t *testing.T) {
+	cache := cacher.NewMemoryCache()
+
+	if !acceptCacher(cache) {
+		t.Errorf("Expected MemoryCache to be accepted")
+		t.FailNow()
+	}
+}
