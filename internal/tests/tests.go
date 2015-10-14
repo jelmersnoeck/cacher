b56ed4e387157197ca76754d00af5627a10f6199
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-10-14 21:26:04 +0100

    Tests: use test driver table.

---
+++ internal/tests/tests.go
@@ -0,0 +1,46 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be found
+// in the LICENSE file.
+
+// Package tests provides helper methods for testing
+package tests
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+
+	"github.com/jelmersnoeck/cacher"
+	"github.com/jelmersnoeck/cacher/internal/numbers"
+)
+
+func Compare(t *testing.T, cache cacher.Cacher, key string, value interface{}) {
+	_, ok := value.(int)
+	if ok {
+		val := int64(value.(int))
+		v, _ := cache.Get(key)
+		valInt, _ := numbers.BytesInt64(v)
+		if valInt != val {
+			msg := "Expected `" + key + "` to equal `" + strconv.FormatInt(val, 10) + "`, is `" + strconv.FormatInt(valInt, 10) + "`"
+			FailMsg(t, cache, msg)
+		}
+	} else {
+		value = value.(string)
+		if v, _ := cache.Get(key); string(v) != value {
+			msg := "Expected `" + key + "` to equal `" + value.(string) + "`"
+			FailMsg(t, cache, msg)
+		}
+	}
+}
+
+func NotPresent(t *testing.T, cache cacher.Cacher, key string) {
+	if _, ok := cache.Get(key); ok {
+		FailMsg(t, cache, "Expected `"+key+"` not to be present")
+	}
+}
+
+func FailMsg(t *testing.T, cache cacher.Cacher, msg string) {
+	errMsg := reflect.TypeOf(cache).String() + ": " + msg
+	t.Errorf(errMsg)
+	t.FailNow()
+}
--- memory_test.go
+++ memory_test.go
@@ -1,15 +1,15 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be found
+// in the LICENSE file.
+
 package cacher_test
 
 import (
 	"testing"
 
 	"github.com/jelmersnoeck/cacher"
-	"github.com/jelmersnoeck/cacher/internal/tester"
+	"github.com/jelmersnoeck/cacher/internal/tests"
 )
-
-func TestMemoryCollection(t *testing.T) {
-	tester.RunCacher(t, cacher.NewMemoryCache(0))
-}
 
 func TestLimit(t *testing.T) {
 	cache := cacher.NewMemoryCache(30)
@@ -20,27 +20,27 @@ func TestLimit(t *testing.T) {
 	cache.Add("key4", []byte("value4"), 0)
 	cache.Add("key5", []byte("value5"), 0)
 
-	tester.Compare(t, cache, "key1", "value1")
-	tester.Compare(t, cache, "key2", "value2")
-	tester.Compare(t, cache, "key3", "value3")
-	tester.Compare(t, cache, "key4", "value4")
-	tester.Compare(t, cache, "key5", "value5")
+	tests.Compare(t, cache, "key1", "value1")
+	tests.Compare(t, cache, "key2", "value2")
+	tests.Compare(t, cache, "key3", "value3")
+	tests.Compare(t, cache, "key4", "value4")
+	tests.Compare(t, cache, "key5", "value5")
 
 	cache.Add("key6", []byte("value6"), 0)
 
-	tester.NotPresent(t, cache, "key1")
+	tests.NotPresent(t, cache, "key1")
 
 	cache.Delete("key3")
 	cache.Add("key7", []byte("value7"), 0)
-	tester.Compare(t, cache, "key2", "value2")
+	tests.Compare(t, cache, "key2", "value2")
 
 	cache.Add("key8", []byte("value8"), 0)
 
 	// This is key5 due to the fact that we fetched key2 before. This pushed it
 	// back as the most active key, so it wouldn't be deleted immediately.
-	tester.NotPresent(t, cache, "key4")
+	tests.NotPresent(t, cache, "key4")
 
 	cache.Add("key9", []byte("value9"), 0)
 
-	tester.NotPresent(t, cache, "key5")
+	tests.NotPresent(t, cache, "key5")
 }
